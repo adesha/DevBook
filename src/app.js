b405955b7fe5def3895669f48af3eebdc7e80461
adesha <adeshaholagodu@gmail.com>
2025-01-11 16:09:22 +0530

    added CRUD

--- src/app.js
+++ src/app.js
@@ -2,13 +2,32 @@ const express=require('express')
 
 const app=express()
 
+// this is wrong (order matters)
+// app.use('/user',(req,res)=>{
+//     res.send('Testing')
+// })
+
+// this will only handle GET call to /user
+app.get('/user',(req,res)=>{
+    res.send({firstName:'John',lastName:'Doe'})
+})
+
+app.post('/user',(req,res)=>{
+    res.send('Saved to DB')
+})
+
+app.delete('/user',(req,res)=>{
+    res.send('Deleted')
+})
+
+// this will match all the http method api calls to /test
 app.use('/test',(req,res)=>{
     res.send('Testing')
 })
 
-app.use((req,res)=>{
-    res.send("Hello")
-})
+// app.use((req,res)=>{
+//     res.send("Hello")
+// })
 
 
 app.listen(3000,()=>{
