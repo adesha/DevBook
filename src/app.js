08ded8aa82cbc70ffac5927ee75a67446cc6a479
adesha <adeshaholagodu@gmail.com>
2025-01-12 11:53:19 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -4,30 +4,41 @@ const app=express()
 
 // this is wrong (order matters)
 // app.use('/user',(req,res)=>{
+    //route handler
 //     res.send('Testing')
 // })
 
 // this will only handle GET call to /user
-app.get('/user',(req,res)=>{
-    res.send({firstName:'John',lastName:'Doe'})
-})
-
-app.post('/user',(req,res)=>{
-    res.send('Saved to DB')
-})
-
-app.delete('/user',(req,res)=>{
-    res.send('Deleted')
-})
-
-// this will match all the http method api calls to /test
-app.use('/test',(req,res)=>{
-    res.send('Testing')
-})
-
-// app.use((req,res)=>{
-//     res.send("Hello")
+// app.get('/user',(req,res)=>{
+//     res.send({firstName:'John',lastName:'Doe'})
 // })
+
+// app.post('/user',(req,res)=>{
+//     res.send('Saved to DB')
+// })
+
+// app.delete('/user',(req,res)=>{
+//     res.send('Deleted')
+// })
+
+// // this will match all the http method api calls to /test
+// app.use('/test',(req,res)=>{
+//     res.send('Testing')
+// })
+
+app.use('/user',
+    (req,res,next)=>{
+        //if no res.send then it will go infinite loop
+        console.log('response')
+        //res.send("response")
+        //returns 'response'
+        next()//goes to next route handler
+    },
+    (req,res)=>{
+        console.log('response2')
+        res.send("response2")
+    }
+)
 
 
 app.listen(3000,()=>{
