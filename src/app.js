842712308369ac117760235b2647573e7216a7dc
adesha <adeshaholagodu@gmail.com>
2025-01-12 18:23:06 +0530

    added signup post api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const express=require('express')
-const connectDB=require('../config/database')
+const connectDB=require('./config/database')
 const app=express()
+const User=require('./models/user')
 
+app.post('/signup',async(req,res)=>{
+
+    const user=new User({
+        firstName:'Mike',
+        lastName:'Ross',
+        email:'mikeross@mail.com',
+        password:'mike123'
+    })
+
+    await user.save()
+    
+    res.send('User Added successfully')
+})
 
 connectDB().then(()=>{
     console.log('Database connected')
